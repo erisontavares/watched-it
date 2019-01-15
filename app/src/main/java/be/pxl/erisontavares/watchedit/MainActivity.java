e1d3a3b09b9fd9d1c35eaca307408732393874e5
Erison Tavares <erison_8@hotmail.com>
2019-01-15 18:39:46 +0100

    Adds Movies and Series activities

--- app/src/main/java/be/pxl/erisontavares/watchedit/MainActivity.java
+++ app/src/main/java/be/pxl/erisontavares/watchedit/MainActivity.java
@@ -1,13 +1,32 @@
 package be.pxl.erisontavares.watchedit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mMoviesButton;
+    private Button mSeriesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMoviesButton = findViewById(R.id.button_movies);
+        mSeriesButton = findViewById(R.id.button_series);
+    }
+
+    public void onMoviesButtonClick(View view) {
+        Intent moviesIntent = new Intent(MainActivity.this, MoviesActivity.class);
+        startActivity(moviesIntent);
+    }
+
+    public void onSeriesButtonClick(View view) {
+        Intent seriesIntent = new Intent(MainActivity.this, SeriesActivity.class);
+        startActivity(seriesIntent);
     }
 }
